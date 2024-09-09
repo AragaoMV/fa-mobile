dbeb4fd5ab239b843be581fe3babab1a6731028a
AragaoMV <marcosvinisa@icloud.com>
2024-09-09 15:29:20 -0300

    Configurar Material Top Tab Navidator e listar posição

--- src/screens/PosicaoPage/index.js
+++ src/screens/PosicaoPage/index.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { onValue, ref } from "firebase/database";
+import { db } from "../../database/config"; 
 import styles from "./styles";
 
 export default function PosicaoPage() {
     const estilos = styles
+    const [todoData, setTodoData] = useState([])
+    useEffect(() => {
+        const starCountRef = ref(db, 'posicao/' + 'ataque/');
+        onValue(starCountRef, (snapshot) => {
+            const data = snapshot.val();
+            const newConceito = Object.keys(data).map(key => ({
+                id: key,
+                ...data[key]
+
+            }));
+            setTodoData(newConceito);
+        })
+    }, [])
+
+
+
     return (
         <View style={estilos.container} >
             <Text style={estilos.texto}>Posicao Page</Text>
+            <View>
+                {
+                    todoData.map((item, index) => {
+                        return (
+                            <View key={index}>
+                                <Text>{item.titulo}</Text>
+                            </View>
+                        )
+                    })
+                }
+            </View>
         </View>
     );
 }
