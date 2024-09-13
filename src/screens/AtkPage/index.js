1837035e12f03bb84cd0fad4d59b6fb3ac4af733
AragaoMV <marcosvinisa@icloud.com>
2024-09-13 15:51:09 -0300

    Troca de View para Scroll view nas telas de conceito

--- src/screens/AtkPage/index.js
+++ src/screens/AtkPage/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView} from "react-native";
 import styles from "./styles";
 import Topo from "./components/Topo";
 import Lista from "./components/Lista";
@@ -7,9 +7,9 @@ import Lista from "./components/Lista";
 export default function AtkPage() {
     const estilos = styles
     return (
-        <View style={estilos.container} >
+        <ScrollView style={estilos.container} >
             <Topo/>
             <Lista/>
-        </View>
+        </ScrollView>
     );
 }
--- src/screens/SpcPage/index.js
+++ src/screens/SpcPage/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView } from "react-native";
 import styles from "./styles";
 import Topo from "./components/Topo";
 import Lista from "./components/Lista";
@@ -7,9 +7,9 @@ import Lista from "./components/Lista";
 export default function SpcPage() {
     const estilos = styles
     return (
-        <View style={estilos.container} >
-            <Topo/>
-            <Lista/>
-        </View>
+        <ScrollView style={estilos.container} >
+            <Topo />
+            <Lista />
+        </ScrollView>
     );
 }
