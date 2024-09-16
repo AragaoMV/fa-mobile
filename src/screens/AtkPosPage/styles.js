1fce329f0c72f203455221adc9155f750b635c9e
AragaoMV <marcosvinisa@icloud.com>
2024-09-16 14:07:02 -0300

    Aplicação da de lista e design da pagina de lista em "Posições de ataque"

--- src/screens/AtkPosPage/styles.js
+++ src/screens/AtkPosPage/styles.js
@@ -4,15 +4,8 @@ import Colors from "../../constants/colors";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent:"center",
         backgroundColor: Colors.neutral500,
     },
-    texto:{
-        fontFamily: 'Roboto_400Regular',
-        fontSize: 20,
-        color:Colors.neutral100
-    }
 });
 
 export default styles;
