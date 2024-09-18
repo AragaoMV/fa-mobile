2706b74e9c0ca03c81971aa792c72f70d5fcad0d
AragaoMV <marcosvinisa@icloud.com>
2024-09-18 09:56:33 -0300

    Adicionado o botão de modo Leitura

--- src/screens/DetailsPage/index.js
+++ src/screens/DetailsPage/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import styles from './styles';
+import { MaterialIcons } from '@expo/vector-icons';
+import Colors from '../../constants/colors';
 
 export default function DetailsPage({ route }) {
   const { titulo, img, detalhes } = route.params;
+  const [modoLeitura, setModoLeitura] = useState(false);
   const estilos = styles;
 
+  //Função para o modoLeitura
+  const iniciaModoLeitura = () => {
+    setModoLeitura(!modoLeitura)
+  }
+
   return (
-    <View style={estilos.container}>
-      <Text style={estilos.title}>{titulo}</Text>
+    <ScrollView style={modoLeitura ? estilos.containerModoLeitura : estilos.container}>
+      <Text style={modoLeitura ? estilos.titleModoLeitura : estilos.title}>{titulo}</Text>
       <Image source={{ uri: img }} style={estilos.image} />
-      <Text style={estilos.description}>{detalhes}</Text>
-    </View>
+      <View style={estilos.botaoModoLeitura}>
+        <TouchableOpacity style={estilos.modoLeitura} onPress={iniciaModoLeitura}>
+          <MaterialIcons name="import-contacts" size={32} color={Colors.main500} />
+        </TouchableOpacity>
+      </View>
+      <Text style={modoLeitura ? estilos.descriptionModoLeitura : estilos.description}>{detalhes}</Text>
+    </ScrollView>
   );
 }
 
--- src/screens/DetailsPage/styles.js
+++ src/screens/DetailsPage/styles.js
@@ -1,35 +1,69 @@
 import { StyleSheet } from "react-native";
-import Colors from "../../constants/colors"; 
+import Colors from "../../constants/colors";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-      backgroundColor: Colors.neutral500,
-    },
-    title: {
-      fontSize: 32,
-      fontWeight: 'bold',
-      marginBottom: 16,
-      color: Colors.main500,
-      textAlign:"center"
-    },
-    image: {
-      width: '94%',
-      alignSelf: "center",
-      height: 192,
-      borderRadius: 8,
-      marginBottom: 16,
-      resizeMode: "cover",
-    },
-    description: {
-      color: Colors.neutral100,
-      textAlign: "justify",
-      fontSize: 16,
-      fontStyle: "normal",
-      fontFamily: 'Roboto_400Regular',
-      lineHeight: 18,
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: Colors.neutral500, // Normal mode background color
+  },
+  containerModoLeitura: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: Colors.paperwhite, // Modo Leitura background color
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: Colors.main500, // Normal mode title color
+    textAlign: "center",
+  },
+  titleModoLeitura: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: Colors.neutral500, // Modo Leitura title color
+    textAlign: "center",
+  },
+  image: {
+    width: '94%',
+    alignSelf: "center",
+    height: 192,
+    borderRadius: 8,
+    marginBottom: 16,
+    resizeMode: "cover",
+  },
+  description: {
+    color: Colors.neutral100, // Normal mode description color
+    textAlign: "justify",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 18,
+  },
+  descriptionModoLeitura: {
+    color: Colors.neutral500, // Modo Leitura description color
+    textAlign: "justify",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 18,
+  },
+  botaoModoLeitura: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+  },
+  modoLeitura: {
+    borderWidth: 2,
+    borderColor: Colors.main500,
+    borderRadius: 40,
+    width: 54,
+    height: 54,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+});
 
 export default styles;
