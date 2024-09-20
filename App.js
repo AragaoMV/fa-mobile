192dc3c4437c0bda080e8dc4444493a41d5e91e4
AragaoMV <marcosvinisa@icloud.com>
2024-09-20 11:18:34 -0300

    Implementação da Splash Screen

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import * as React from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { NavigationContainer } from "@react-navigation/native";
-import Fonts from "./src/constants/fonts";
+import Fonts from "./src/constants/fonts"; // Hook de fontes
 import DrawerNavigator from "./src/navigation/DrawerNavigator";
-
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
-  const fontsLoaded = Fonts();
+  const fontsLoaded = Fonts(); // Carregamento das fontes
+
+  React.useEffect(() => {
+    async function prepare() {
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync(); // Esconde a Splash Screen quando as fontes estiverem carregadas
+      }
+    }
+    prepare();
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
-    return <Text>Loading...</Text>;
+    // A Splash Screen vai cobrir isso, então não precisa renderizar nada
+    return null;
   }
 
   return (
